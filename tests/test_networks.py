11e26f72d56fafed1b87f440356faf422701b395
Matbhansen <maitrox@live.com>
2026-02-06 11:31:24 +0100

    Added more test for create tree graph

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -2,8 +2,7 @@ from patient_zero.networks import (create_tree_graph as ctg)
 import networkx as nx
 
 class TestCreateTreeGraph:
-    def test_create_tree_graph(self):
-
+    def test_create_small_tree_graph(self):
         # Arrange
         Tree = ctg(2,1)
 
@@ -14,3 +13,31 @@ class TestCreateTreeGraph:
         # Assert
         assert nodes == [0, 1, 2]
         assert edges == [(0, 1), (0, 2)]
+    
+    def test_create_big_depth_tree_graph(self):
+        # Arrange
+        Tree = ctg(2,10)
+
+        # Act
+        nodes = Tree.number_of_nodes()
+        edges = Tree.number_of_edges()
+
+        print(nodes)
+        print(edges)
+        # Assert
+        assert nodes == 2047
+        assert edges == 2046
+
+    def test_create_big_breadth_tree_graph(self):
+        # Arrange
+        Tree = ctg(100,2)
+
+        # Act
+        nodes = Tree.number_of_nodes()
+        edges = Tree.number_of_edges()
+
+        print(nodes)
+        print(edges)
+        # Assert
+        assert nodes == 10101
+        assert edges == 10100
