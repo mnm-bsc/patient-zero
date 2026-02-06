67efc0b1c05837b2f43612abc99cb565ee1e48c6
Matbhansen <maitrox@live.com>
2026-02-06 10:47:54 +0100

    Added create tree graph test

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -1,3 +1,7 @@
-from patient_zero.networks import (create_tree_graph as ctree)
+from patient_zero.networks import (create_tree_graph as ctg)
 import networkx as nx
 
+class TestCreateTreeGraph:
+    def test_create_tree_graph():
+        tree = ctg(2,2)
+        assert True
