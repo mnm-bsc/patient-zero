6dcf7276d710c296ce625bddcb6b0fd9f050e080
Matbhansen <maitrox@live.com>
2026-02-06 11:38:26 +0100

    finalized test for create tree graph

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -2,7 +2,17 @@ from patient_zero.networks import (create_tree_graph as ctg)
 import networkx as nx
 
 class TestCreateTreeGraph:
-    def test_create_small_tree_graph(self):
+    def test_create_tree_graph_for_emtpy_tree(self):
+        # Arrange
+        Tree = ctg(0,0)
+
+        # Act
+        nodes = list(Tree.nodes())
+
+        # Assert
+        assert nodes == [0]
+
+    def test_create_tree_graph_for_small_tree(self):
         # Arrange
         Tree = ctg(2,1)
 
@@ -14,7 +24,7 @@ class TestCreateTreeGraph:
         assert nodes == [0, 1, 2]
         assert edges == [(0, 1), (0, 2)]
     
-    def test_create_big_depth_tree_graph(self):
+    def test_create_tree_graph_with_big_depth(self):
         # Arrange
         Tree = ctg(2,10)
 
@@ -22,13 +32,11 @@ class TestCreateTreeGraph:
         nodes = Tree.number_of_nodes()
         edges = Tree.number_of_edges()
 
-        print(nodes)
-        print(edges)
         # Assert
         assert nodes == 2047
         assert edges == 2046
 
-    def test_create_big_breadth_tree_graph(self):
+    def test_create_tree_graph_with_big_breadth(self):
         # Arrange
         Tree = ctg(100,2)
 
@@ -36,8 +44,6 @@ class TestCreateTreeGraph:
         nodes = Tree.number_of_nodes()
         edges = Tree.number_of_edges()
 
-        print(nodes)
-        print(edges)
         # Assert
         assert nodes == 10101
         assert edges == 10100
