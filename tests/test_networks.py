931538610ddb55324294fb2ce31cb7059f50001a
Matbhansen <maitrox@live.com>
2026-02-06 11:09:57 +0100

    added AAA comments to test

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -4,10 +4,13 @@ import networkx as nx
 class TestCreateTreeGraph:
     def test_create_tree_graph(self):
 
-        
+        # Arrange
         Tree = ctg(2,1)
 
+        # Act
         nodes = list(Tree.nodes())
         edges = list(Tree.edges())
 
+        # Assert
         assert nodes == [0, 1, 2]
+        assert edges == [(0, 1), (0, 2)]
