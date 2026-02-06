d95418d8b928fe53c2ffb5214c9ed1a4e456e29d
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2026-02-06 11:53:02 +0100

    fix lint

--- src/patient_zero/models.py
+++ src/patient_zero/models.py
@@ -5,7 +5,7 @@ Implementation of cascade models for simulating spreading processes.
 import random
 import networkx as nx
 
-def independent_cascade(G: nx.Graph, patient_zero: int, r: float, max_steps: int = None):
+def independent_cascade(g: nx.Graph, patient_zero: int, r: float, max_steps: int = None):
     """Implementation of IC model"""
     infected_nodes = {patient_zero}
     all_infected = set(infected_nodes)
@@ -19,7 +19,7 @@ def independent_cascade(G: nx.Graph, patient_zero: int, r: float, max_steps: int
         new_infected = set()
 
         for node in infected_nodes:
-            for neighbor in G.neighbors(node):
+            for neighbor in g.neighbors(node):
                 if neighbor not in all_infected:
                     if random.random() < r:
                         new_infected.add(neighbor)
@@ -30,10 +30,10 @@ def independent_cascade(G: nx.Graph, patient_zero: int, r: float, max_steps: int
     
     return all_infected, step
 
-def susceptible_infected_recovered(G: nx.graph, patient_zero: int, r_infect: float, r_recover, max_steps: int = None):
+def susceptible_infected_recovered(g: nx.graph, patient_zero: int, r_infect: float, r_recover, max_steps: int = None):
     """Implementation of SIR model"""
     infected_nodes = {patient_zero}
-    susceptible_nodes = set(G.nodes()) - infected_nodes
+    susceptible_nodes = set(g.nodes()) - infected_nodes
     recovered_nodes = set()
     step = 1
 
@@ -44,7 +44,7 @@ def susceptible_infected_recovered(G: nx.graph, patient_zero: int, r_infect: flo
         new_infected = set()
 
         for node in infected_nodes:
-            for neighbor in G.neighbors(node):
+            for neighbor in g.neighbors(node):
                 if neighbor in susceptible_nodes and random.random() < r_infect:
                     susceptible_nodes.remove(neighbor)
                     new_infected.add(neighbor)
--- src/patient_zero/networks.py
+++ src/patient_zero/networks.py
@@ -2,13 +2,15 @@
 import networkx as nx
 
 def create_simple_graph():
-    G=nx.Graph() # Create an empty graph
-    G.add_nodes_from([1,2,3,4]) # Add nodes from 1-4    
-    G.add_edges_from([(1,2),(1,3),(1,4)]) # Add edges
+    """Test"""
+    g=nx.Graph() # Create an empty graph
+    g.add_nodes_from([1,2,3,4]) # Add nodes from 1-4    
+    g.add_edges_from([(1,2),(1,3),(1,4)]) # Add edges
 
 create_simple_graph()
 
 def create_tree_graph(children, depth):
+    """Creates a simple tree graph with a specified number of children and depth"""
     if depth < 0: # Return if depth is negative
         return print("Not a possible graph")
     
