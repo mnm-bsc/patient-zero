159f2bf54be524e9b451bee237beb1b80b4e4101
MadsOstrup <moes@itu.dk>
2026-02-06 11:08:35 +0100

    more test cases for SIR

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,12 +10,23 @@ class TestSusceptibleInfectedRecovered:
     def create_tree(self):
         return create_tree_graph(12, 5)
     
-    def test_More_than_zero_recovered(self):
+    def test_more_than_zero_recovered(self):
         tree = self.create_tree()
-        susceptible, infected, recovered = sir(tree, 0, 0.3, 0.2)
+        _, _, recovered = sir(tree, 0, 0.3, 0.2)
         assert len(recovered) > 0
 
-    def test_SIR(self):
+    def test_susceptible_infected_recovered_adds_up_to_all_nodes(self):
         tree = self.create_tree()
         susceptible, infected, recovered = sir(tree, 0, 0.3, 0.2)
+        assert len(susceptible) < len(tree.nodes())
         assert len(susceptible) + len(infected) + len(recovered) == len(tree.nodes())
+    
+    def test_no_infected_after_simulation(self):
+        tree = self.create_tree()
+        _, infected, _ = sir(tree, 0, 0.3, 0.2)
+        assert len(infected) == 0
+
+    def test_more_than_0_infected_after_few_steps(self):
+        tree = self.create_tree()
+        _, infected, _ = sir(tree, 0, 0.3, 0.2, 2)
+        assert len(infected) > 0
