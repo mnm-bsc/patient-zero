66016291f5564e6212944a1bd722d1f14cd6cf33
Matbhansen <maitrox@live.com>
2026-02-06 11:08:14 +0100

    Fixed test

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -3,5 +3,11 @@ import networkx as nx
 
 class TestCreateTreeGraph:
     def test_create_tree_graph(self):
-        tree = ctg(2,2)
-        assert True
+
+        
+        Tree = ctg(2,1)
+
+        nodes = list(Tree.nodes())
+        edges = list(Tree.edges())
+
+        assert nodes == [0, 1, 2]
