8765e5e2cc72bcbc5a8aff9c6166867f5d403f6a
Matbhansen <maitrox@live.com>
2026-02-06 15:15:21 +0100

    Added test for random graph with 4 nodes

--- tests/test_networks/test_random_graph.py
+++ tests/test_networks/test_random_graph.py
@@ -4,8 +4,8 @@ from patient_zero.networks import create_random_graph as crg
 class TestCreateRandomGraph:
     """Test the create_random_graph function"""
 
-    def test_create_random_graph_for_empty_graph(self):
-        """Test the create_random_graph(0,0) function for an empty graph"""
+    def test_create_random_graph_for_an_empty_graph(self):
+        """Test the create_random_graph() function for an empty graph"""
 
         # Arrange
         g = crg(0,0)
@@ -15,3 +15,35 @@ class TestCreateRandomGraph:
 
         # Assert
         assert nodes == []
+
+    def test_create_random_graph_with_4_nodes(self):
+        """Test the create_random_graph() function to create a graph with 4 nodes"""
+
+        # Arrange
+        g = crg(4,0.5)
+
+        # Act
+        num_nodes = g.number_of_nodes()
+        nodes = list(g.nodes())
+
+        print(g)
+        print(nodes)
+        # Assert
+        assert num_nodes == 4
+        assert nodes == [0, 1, 2, 3]
+
+    def test_create_random_graph_with_no_edges(self):
+        """Test the create_random_graph() function to create a graph with no edges"""
+
+        # Arrange
+        g = crg(4,0.5)
+
+        # Act
+        num_nodes = g.number_of_nodes()
+        nodes = list(g.nodes())
+
+        print(g)
+        print(nodes)
+        # Assert
+        assert num_nodes == 4
+        assert nodes == [0, 1, 2, 3]
