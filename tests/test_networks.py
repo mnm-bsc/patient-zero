0e9db8f3694f60a2491bec36569c7cad14cc3089
Matbhansen <maitrox@live.com>
2026-02-06 10:49:02 +0100

    fixed test

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -2,6 +2,6 @@ from patient_zero.networks import (create_tree_graph as ctg)
 import networkx as nx
 
 class TestCreateTreeGraph:
-    def test_create_tree_graph():
+    def test_create_tree_graph(self):
         tree = ctg(2,2)
         assert True
