cfd68b1dd4df6bded197c58c57a376e146d017a7
Matbhansen <maitrox@live.com>
2026-02-13 09:29:55 +0100

    Added test file for scale-free graph

---
+++ tests/test_networks/test_scale_free.py
@@ -0,0 +1,5 @@
+"""Tests for creating scale-free graphs"""
+from patient_zero.networks import create_scale_free_graph as csfg
+
+class TestCreateScaleFreeGraph:
+    """Test the create_small_world_graph function"""
