4454e1434947db1ed64cf1e5811a5d58fc02e754
Matbhansen <maitrox@live.com>
2026-02-13 09:23:49 +0100

    Finished test for small-world graph

--- tests/test_networks/test_small_world.py
+++ tests/test_networks/test_small_world.py
@@ -15,3 +15,45 @@ class TestCreateSmallWorldGraph:
 
         # Assert
         assert not nodes
+    
+    def test_create_small_world_graph_for_a_small_graph(self):
+        """Test the create_small_world_graph() function to create a small graph"""
+
+        # Arrange
+        g = cswg(10,2,0)
+
+        # Act
+        num_nodes = g.number_of_nodes()
+        num_edges = g.number_of_edges()
+        
+        # Assert
+        assert num_nodes == 10 
+        assert num_edges == 10
+
+    def test_create_small_world_graph_with_no_edges(self):
+        """Test the create_small_world_graph() function to create a graph with no edges"""
+
+        # Arrange
+        g = cswg(10,0,0)
+
+        # Act
+        num_nodes = g.number_of_nodes()
+        num_edges = g.number_of_edges()
+        
+        # Assert
+        assert num_nodes == 10 
+        assert num_edges == 0
+    
+    def test_create_small_world_graph_for_a_big_graph(self):
+        """Test the create_small_world_graph() function to create a graph with no edges"""
+
+        # Arrange
+        g = cswg(1000,4,0.1)
+
+        # Act
+        num_nodes = g.number_of_nodes()
+        num_edges = g.number_of_edges()
+        
+        # Assert
+        assert num_nodes == 1000
+        assert num_edges == 2000
