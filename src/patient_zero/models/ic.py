687318272c783accf8e0923228371eb8983db582
Matbhansen <maitrox@live.com>
2026-02-18 12:42:54 +0100

    updated ic

--- src/patient_zero/models/ic.py
+++ src/patient_zero/models/ic.py
@@ -12,22 +12,18 @@ def independent_cascade(g: nx.Graph, patient_zero: int, r: float, max_size: int 
     infected = {patient_zero}
     all_infected = set(infected)
     cascade_edges = []
-    
-    size = len(all_infected)
 
-    while infected and (max_size is None or size < max_size):
-
+    while infected:
         new_infected = set()
         for node in infected:
             for neighbor in sorted(g.neighbors(node)):
                 if neighbor not in all_infected and rng.random() < r:
-                    if size >= max_size:
+                    if (max_size is not None and len(all_infected) >= max_size):
                         return all_infected, cascade_edges
 
                     new_infected.add(neighbor)
                     all_infected.add(neighbor)
                     cascade_edges.append((node, neighbor))
-                    size = len(all_infected)
         infected = new_infected
     
     return all_infected, cascade_edges
