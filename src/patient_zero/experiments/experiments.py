034d0a08ff36bbe79bd825ef64c2471508f5a066
Matbhansen <maitrox@live.com>
2026-02-18 10:27:58 +0100

    use enum instead

--- src/patient_zero/experiments/experiments.py
+++ src/patient_zero/experiments/experiments.py
@@ -5,6 +5,7 @@ import os
 from patient_zero.networks import create_tree_graph, create_random_graph, create_scale_free_graph, create_small_world_graph
 from patient_zero.models import ic, sir
 from patient_zero.networks.utils import get_random_node
+from patient_zero.enums import NetworkType, ModelType
 
 
 #def run_ic_simulation(graph, r, max_steps=None):
@@ -13,13 +14,13 @@ from patient_zero.networks.utils import get_random_node
 #def run_sir_simulation(graph, r, recovery, max_steps=None):
 
 def get_graph(type: str, params: any):
-    if type == "erdos_renyi":
+    if type == NetworkType.RANDOM:
         return create_random_graph(params.nodes, params.probability, params.seed)
-    elif type == "watts_strogatz":
+    elif type == NetworkType.SMALL_WORLD:
         return create_small_world_graph(params.nodes, params.neighbors, params.probability, params.seed)
-    elif type == "barabasi_ablert":
+    elif type == NetworkType.SCALE_FREE:
         return create_scale_free_graph(params.nodes, params.edges, params.seed)
-    elif type == "balanced_tree":
+    elif type == NetworkType.TREE:
         return create_tree_graph(params.children, params.depth)
     else: 
         raise ValueError("incorrect graph type")
@@ -32,7 +33,7 @@ def main():
         print(metadata)
     for graph in metadata.graphs:
         g = get_graph(graph.type, graph.params)
-        
+
    
 
 if __name__ == "__main__":
