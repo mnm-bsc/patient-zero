357dbe36a603b1f22328cc1bc669f0d67e93a395
Matbhansen <maitrox@live.com>
2026-02-18 12:36:45 +0100

    added size to ic

--- src/patient_zero/models/ic.py
+++ src/patient_zero/models/ic.py
@@ -5,7 +5,7 @@ Independent cascade model
 import random
 import networkx as nx
 
-def independent_cascade(g: nx.Graph, patient_zero: int, r: float, max_steps: int = None, seed: int = None):
+def independent_cascade(g: nx.Graph, patient_zero: int, r: float, max_size: int = None, seed: int = None):
     """Implementation of IC model"""
     rng = random.Random(seed)
 
@@ -13,18 +13,21 @@ def independent_cascade(g: nx.Graph, patient_zero: int, r: float, max_steps: int
     all_infected = set(infected)
     cascade_edges = []
     
-    step = 0
+    size = len(all_infected)
 
-    while infected and (max_steps is None or step < max_steps):
-        step += 1
+    while infected and (max_size is None or size < max_size):
 
         new_infected = set()
         for node in infected:
             for neighbor in sorted(g.neighbors(node)):
                 if neighbor not in all_infected and rng.random() < r:
+                    if size >= max_size:
+                        break
+                    
                     new_infected.add(neighbor)
                     all_infected.add(neighbor)
                     cascade_edges.append((node, neighbor))
+                    size = len(all_infected)
         infected = new_infected
     
     return all_infected, cascade_edges
