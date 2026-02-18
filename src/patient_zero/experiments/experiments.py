7b633d74bd9ea334e12e8b4415e582bdca4b87d4
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2026-02-18 10:37:52 +0100

    ensure correct params

--- src/patient_zero/experiments/experiments.py
+++ src/patient_zero/experiments/experiments.py
@@ -1,7 +1,6 @@
 """Playground for testing the models and networks"""
 
 import json
-import os
 from patient_zero.networks import create_tree_graph, create_random_graph, create_scale_free_graph, create_small_world_graph
 from patient_zero.models import ic, sir
 from patient_zero.networks.utils import get_random_node
@@ -13,17 +12,33 @@ from patient_zero.enums import NetworkType, ModelType
 
 #def run_sir_simulation(graph, r, recovery, max_steps=None):
 
-def get_graph(type: str, params: any):
+def get_graph(type: NetworkType, params: any):
     if type == NetworkType.RANDOM:
-        return create_random_graph(params.nodes, params.probability, params.seed)
+        return create_random_graph(
+            nodes=params.nodes,
+            probability=params.probability,
+            seed=params.seed
+        )
     elif type == NetworkType.SMALL_WORLD:
-        return create_small_world_graph(params.nodes, params.neighbors, params.probability, params.seed)
+        return create_small_world_graph(
+            nodes=params.nodes,
+            neighbors=params.neighbors,
+            probability=params.probability,
+            seed=params.seed
+        )
     elif type == NetworkType.SCALE_FREE:
-        return create_scale_free_graph(params.nodes, params.edges, params.seed)
+        return create_scale_free_graph(
+            nodes=params.nodes,
+            edges=params.edges,
+            seed=params.seed
+        )
     elif type == NetworkType.TREE:
-        return create_tree_graph(params.children, params.depth)
+        return create_tree_graph(
+            children=params.children,
+            depth=params.depth
+        )
     else: 
-        raise ValueError("incorrect graph type")
+        raise ValueError(f"Unknown graph type: type={type}")
 
 
 
