ecc31340f96384ff34c16c0bf2173c68bfda0278
Matbhansen <maitrox@live.com>
2026-02-18 12:45:30 +0100

    updated sir

--- src/patient_zero/models/sir.py
+++ src/patient_zero/models/sir.py
@@ -10,7 +10,7 @@ def susceptible_infected_recovered(
         patient_zero: int,
         r_infect: float,
         r_recover,
-        max_steps: int = None, 
+        max_size: int = None, 
         seed: int = None
     ):
     """Implementation of SIR model"""
@@ -23,17 +23,17 @@ def susceptible_infected_recovered(
 
     all_infected = {patient_zero}
     cascade_edges = []
-    
-    step = 0
 
-    while infected and (max_steps is None or step < max_steps):
-        step += 1
+    while infected:
         new_infected = set()
         new_recovered = set()
 
         for node in sorted(infected):
             for neighbor in sorted(g.neighbors(node)):
                 if neighbor in susceptible and rng.random() < r_infect:
+                    if (max_size is not None and len(all_infected) >= max_size):
+                        return all_infected, cascade_edges
+                    
                     new_infected.add(neighbor)
                     all_infected.add(neighbor)
                     cascade_edges.append((node, neighbor))
