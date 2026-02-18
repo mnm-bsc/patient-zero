e6fa8e4d86df1de7f140c210a8f0856db8c3198e
Matbhansen <maitrox@live.com>
2026-02-18 09:39:48 +0100

    added data file and json metadata file

---
+++ src/patient_zero/experiments/experiments.py
@@ -0,0 +1,19 @@
+"""Playground for testing the models and networks"""
+
+import time
+from patient_zero.networks import create_tree_graph, create_random_graph
+from patient_zero.models import ic, sir
+from patient_zero.networks.utils import get_random_node
+
+
+def run_ic_simulation(graph, r, max_steps=None):
+    
+
+def run_sir_simulation(graph, r, recovery, max_steps=None):
+    
+
+def main():
+   
+
+if __name__ == "__main__":
+    main()
