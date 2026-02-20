7c70b68039c3871e0a542268a694f4555edd211f
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2026-02-20 11:23:43 +0100

    save to csb

--- src/patient_zero/experiments/experiments.py
+++ src/patient_zero/experiments/experiments.py
@@ -1,29 +1,12 @@
 import networkx as nx
+import pandas as pd
+import json
 from collections.abc import Callable
 from pathlib import Path
 from patient_zero.experiments.utils import pkl_to_cascade
 from patient_zero.experiments.centrality import degree_centrality, eigenvector_centrality, distance_centrality
 
-data_dir = Path("simulations")
-
-def test():
-    simulation = pkl_to_cascade("simulations/erdos_renyi/IC/erdos_renyi_IC_cascade50.pkl")["erdos_renyi_IC_cascade50_r0.2"]
-    cascade = simulation.get("cascade")
-    patient_zero = simulation.get("patient_zero")
-
-    degree = degree_centrality(cascade)
-    eigenvector = eigenvector_centrality(cascade)
-    distance = distance_centrality(cascade)
-
-    guess = max(degree, key=degree.get)
-    print(f"degree guess: {guess}, patient zero: {patient_zero}, diff: {nx.shortest_path_length(cascade, guess, patient_zero)}")
-
-    guess = max(eigenvector, key=eigenvector.get)
-    print(f"eigenvector guess: {guess}, patient zero: {patient_zero}, diff: {nx.shortest_path_length(cascade, guess, patient_zero)}")
-
-    guess = max(distance, key=distance.get)
-    print(f"distance guess: {guess}, patient zero: {patient_zero}, diff: {nx.shortest_path_length(cascade, guess, patient_zero)}")
-
+DATA_DIR = Path("simulations")
 
 def calculate_centrality(centrality_function: Callable, cascade: nx.Graph, patient_zero: int):
     result = centrality_function(cascade)
@@ -34,10 +17,11 @@ def calculate_centrality(centrality_function: Callable, cascade: nx.Graph, patie
 
 
 def main():
+    # new centrality measures can be added below
     centrality_measures = [degree_centrality, eigenvector_centrality, distance_centrality]
     results = []
 
-    for pkl_file in data_dir.rglob("*.pkl"):
+    for pkl_file in DATA_DIR.rglob("*.pkl"):
         cascades = pkl_to_cascade(pkl_file)
         
         for id, data in cascades.items():
@@ -46,10 +30,17 @@ def main():
 
             for cm in centrality_measures:
                 guess, diff = calculate_centrality(cm, cascade, metadata["patient_zero"])
-                print(f"id={id}, centrality={cm.__name__}, diff={diff}")
+                results.append({
+                    "id": id,
+                    "centrality": cm.__name__,
+                    "guess": guess,
+                    "diff": diff,
+                    **metadata
+                })
 
-            
-
+    # save to csv
+    df = pd.DataFrame(data=results, columns=results[0].keys())
+    df.to_csv("results.csv", index=False)   
 
 if __name__ == "__main__":
     main()
