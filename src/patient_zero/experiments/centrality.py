7d493767e310512a301774526fd1d11432d6962a
Matbhansen <maitrox@live.com>
2026-02-27 12:42:57 +0100

    fixed rumor centrality

--- src/patient_zero/experiments/centrality.py
+++ src/patient_zero/experiments/centrality.py
@@ -28,8 +28,10 @@ def rumor_centrality(cascade: nx.Graph):
         prod = 0
         for tree_node in BFS_tree.nodes: # Computes the product of all subtree sizes.
             prod += math.log(subtree_size[tree_node]) # prod = product of T[u] for all nodes u in the BFS tree.
-        print(prod)
-        if prod == 0.0: continue
+        
+        if prod == 0.0: # If the cascade is one return a default score of 1
+            node_scores[node] = 1
+            return node_scores
         node_scores[node] = -math.log(prod) # Compute root's rumor centrality. R(root) = n! / prod = -ln(prod)
     return node_scores
 
