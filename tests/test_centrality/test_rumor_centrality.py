bebe2399e86ce2cb78f1d75c2f9da7512c96036a
Matbhansen <maitrox@live.com>
2026-04-03 11:47:48 +0200

    Added doc to rumor test

--- tests/test_centrality/test_rumor_centrality.py
+++ tests/test_centrality/test_rumor_centrality.py
@@ -4,6 +4,9 @@ from patient_zero.experiments import dfs, rumor_centrality
 
 class TestRumorCentrality:
     def test_dfs(self):
+        """
+        Testing if the dfs function is correct
+        """
         G = nx.Graph()
         G.add_edges_from([
             (0, 1),
@@ -21,6 +24,9 @@ class TestRumorCentrality:
         assert subtree_size[4] == 1 # Assert 1 because 4 is a leaf
 
     def test_rumor_centrality_score_nodes(self):
+        """
+        Testing the rumor centrality gives scores to nodes
+        """
         G = nx.Graph()
         G.add_edges_from([
             (0, 1),
@@ -33,6 +39,9 @@ class TestRumorCentrality:
         assert set(scores.keys()) == {0, 1, 2, 3, 4}
 
     def test_leaves_has_less_score(self):
+        """
+        Testing if leaves has a lower score than other nodes
+        """
         G = nx.Graph()
         G.add_edges_from([
             (0, 1),
@@ -50,6 +59,9 @@ class TestRumorCentrality:
             assert scores[leaf] < max(scores[c] for c in center)
 
     def test_two_nodes_same_score(self):
+        """
+        Testing if two nodes can have the same score
+        """
         G = nx.Graph()
         G.add_edge(0, 1)
         rumor_scores = rumor_centrality(G)
@@ -57,12 +69,18 @@ class TestRumorCentrality:
         assert rumor_scores[0] == rumor_scores[1]
 
     def test_balanced_tree_root_highest_score(self):
+        """
+        Testing if root node has the highest score in a balanced tree
+        """
         G = nx.balanced_tree(r=2, h=2)
         rumor_scores = rumor_centrality(G)
 
         assert rumor_scores[0] == max(rumor_scores.values())
 
     def test_path_graph_symmetric_scores(self):
+        """
+        Testing if nodes have a symmetrical score on a symmetrical graph
+        """
         G = nx.path_graph(5)
         rumor_scores = rumor_centrality(G)
 
@@ -71,6 +89,9 @@ class TestRumorCentrality:
         assert rumor_scores[1] == rumor_scores[3]
 
     def test_dfs_on_path_graph(self):
+        """
+        Testing if the dfs subtree is correct on a path graph
+        """
         G = nx.path_graph(5)
         tree = nx.bfs_tree(G, source=0)
         subtree = {}
