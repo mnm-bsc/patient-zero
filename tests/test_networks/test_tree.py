f13d7cc26bd9c1427ee64f9bfad69c2cc56af5bb
Matbhansen <maitrox@live.com>
2026-04-22 12:58:15 +0200

    fixed network tests comments

--- tests/test_networks/test_tree.py
+++ tests/test_networks/test_tree.py
@@ -5,55 +5,47 @@ class TestCreateTreeGraph:
     """Test the create_tree_graph function"""
 
     def test_create_tree_graph_for_empty_tree(self):
-        """Test the create_tree_graph() for a empty tree"""
-
-        # Arrange
+        """
+        Test the create_tree_graph() for a empty tree.
+        """
         tree = ctg(0,0)
 
-        # Act
         nodes = list(tree.nodes())
 
-        # Assert
         assert nodes == [0]
 
     def test_create_tree_graph_for_small_tree(self):
-        """Test the create_tree_graph() for a small tree"""
-
-        # Arrange
+        """
+        Test the create_tree_graph() for a small tree.
+        """
         tree = ctg(2,1)
 
-        # Act
         nodes = list(tree.nodes())
         edges = list(tree.edges())
 
-        # Assert
         assert nodes == [0, 1, 2]
         assert edges == [(0, 1), (0, 2)]
     
     def test_create_tree_graph_with_big_depth(self):
-        """Test the create_tree_graph() for a tree with a large depth"""
-
-        # Arrange
+        """
+        Test the create_tree_graph() for a tree with a large depth.
+        """
         tree = ctg(2,10)
 
-        # Act
         nodes = tree.number_of_nodes()
         edges = tree.number_of_edges()
 
-        # Assert
         assert nodes == 2047
         assert edges == 2046
 
     def test_create_tree_graph_with_big_breadth(self):
-        """Test the create_tree_graph() for a tree with a large breadth"""
-
-        # Arrange
+        """
+        Test the create_tree_graph() for a tree with a large breadth.
+        """
         tree = ctg(100,2)
 
-        # Act
         nodes = tree.number_of_nodes()
         edges = tree.number_of_edges()
 
-        # Assert
         assert nodes == 10101
         assert edges == 10100
